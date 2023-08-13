6fa463038b3ec09e17df1c58ded39f73b1d1981e
mlwelker <nutria.watts.0g@icloud.com>
2023-08-12 23:44:53 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Michael Welker on 2023-08-12.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
