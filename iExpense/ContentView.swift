687fba84934dfbee49bfa81871692362ceaff314
mlwelker <nutria.watts.0g@icloud.com>
2023-08-13 22:46:53 -0500

    with JSONencoder

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -1,12 +1,21 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
+
 struct ContentView: View {
-    @AppStorage("tapCount") private var tapCount = 0
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
     
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            tapCount += 1
+        Button("Save user") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
         }
     }
 }
