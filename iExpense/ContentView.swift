5590e71dd59b982c88bef259e82daab84bda1cb8
mlwelker <nutria.watts.0g@icloud.com>
2023-08-15 18:06:39 -0500

    complete + 2 of 3 challenges

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -3,23 +3,38 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items, id: \.id) { item in
-                    Text(item.name)
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .font(item.amount > 10 ? .system(.body, weight: .bold) : .none )
+                            .foregroundColor(item.amount > 100 ? .red : .black)
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
             .navigationTitle("iExpense")
             .toolbar {
                 Button {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 } label: {
                     Image(systemName: "plus")
                 }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
         }
     }
--- iExpense/Expenses.swift
+++ iExpense/Expenses.swift
@@ -2,5 +2,24 @@
 import Foundation
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let data = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(data, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+//        let thing = [ExpenseItem].self
+        
+        if let data = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: data) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
 }
